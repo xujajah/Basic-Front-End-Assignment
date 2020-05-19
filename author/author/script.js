f20a0c61f5b0b5a5d73de3f4defaf82431e137d2
xujajah <58356136+xujajah@users.noreply.github.com>
2020-05-19 21:42:42 +0500

    Add files via upload

---
+++ author/author/script.js
@@ -0,0 +1,6 @@
+function getdata() {
+    var name = document.getElementById("name").value;
+    var mail = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+}
